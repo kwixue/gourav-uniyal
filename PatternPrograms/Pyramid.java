b8ce2b0922dfff0e688625442900a7c429d610c2
kwixue <meiqiaot1557@163.com>
2021-03-29 01:20:33 +0800

    Pyramid with star are completed

--- PatternPrograms/Pyramid.java
+++ PatternPrograms/Pyramid.java
@@ -1,9 +1,9 @@
-public class SpacedPyramid {
+public class Pyramid {
 	public static void main(String[] args) {
 		int n = 5;
 		for (int i = 0; i < n; i++) {
-			for (int j = 0, k = i + 1; j < 2 * n + 1 && k > 0; j++) {
-				if (i + j >= n && (i + j) % 2 == 0) {
+			for (int j = 0, k = 2 * i + 1; j < 2 * n + 1 && k > 0; j++) {
+				if (i + j >= n) {
 					System.out.print("*");
 					k--;
 				} else
